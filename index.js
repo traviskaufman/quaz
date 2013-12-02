201606f1eaae763a20b012d877c01f205004f42c
Travis Kaufman <travis.kaufman@gmail.com>
2013-12-01 22:38:25 -0500

    Implement transform function and hook up simple quaz() function

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-module.exports = function() {};
+var tokenize = require('./lib/tokenize');
+var transform = require('./lib/transform');
+
+/**
+ * The qz function.
+ * @param {Function} handler The string template handler function.
+ * @return {Function} A function that will call the handler with the parsed
+ *  template string.
+ */
+module.exports = function(handler) {
+  // TODO: handle qz(src, handler), qz(src) as well
+  return function(src) {
+    var parsed = transform(tokenize(src));
+    return handler.apply(this, [parsed.literals].concat(parsed.substitutions));
+  };
+};
