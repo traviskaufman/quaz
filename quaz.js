d7b81504a5d018e533d0f209f1207e6a790a84f2
Travis Kaufman <travis.kaufman@gmail.com>
2013-12-02 23:22:21 -0500

    Add escape sequences, acceptance tests. Still doesn't work :(

--- quaz.js
+++ quaz.js
@@ -1,5 +1,6 @@
 var tokenize = require('./lib/tokenize');
 var transform = require('./lib/transform');
+var utils = require('./lib/utils');
 
 /**
  * The qz function.
@@ -14,3 +15,26 @@ module.exports = function(handler) {
     return handler.apply(this, [parsed.literals].concat(parsed.substitutions));
   };
 };
+
+/**
+ * This is the default handler function for qz, and it will be called if a
+ * string is given to quaz without giving a function.
+ * @param {Object} callSite The object holding raw and cooked literals.
+ *
+ * Note that substitutions are given as positional args after callSite.
+ * This is taken from
+ * http://wiki.ecmascript.org/doku.php?id=harmony:quasis#default_quasi_tag.
+ * However, it has been changed to loop through the cooked string instead of
+ * the raw string.
+ */
+module.exports.DEFAULT_HANDLER = utils.freezeIfPossible(function(callSiteId) {
+  var strs = callSiteId;
+  var out = [];
+  var i = 0, k = -1, n = strs.length - 1;
+  while (i < n) {
+    out[++k] = strs[i];
+    out[++k] = arguments[++i];
+  }
+  out[++k] = strs[n];
+  return out.join('');
+});
--- test/quaz.spec.js
+++ test/quaz.spec.js
@@ -1,4 +1,7 @@
 'use strict';
+// We're disabling this because hint will complain about unused variables in
+// quasi-literals
+/* jshint unused: false */
 var qz = require(BASE_DIR);
 
 describe('quaz', function() {
@@ -12,8 +15,36 @@ describe('quaz', function() {
     quasiHandler = qz(handlerFn);
   });
 
-  xit('creates a quasi-literal handler given a handler function', function() {
+  it('creates a quasi-literal handler given a handler function', function() {
     assert.isFunction(quasiHandler);
+  });
+
+  describe('acceptance tests (examples from ES Wiki)', function() {
+
+    it('`foo${bar}baz`', function() {
+      var bar = 'barVar';
+      assert.strictEqual(qz('foo${bar}baz'), 'foobarVarbaz');
+    });
+
+    it('html string (<a href="${url}" ....', function() {
+      var url = 'http://www.example.org',
+          query = 'foo=bar&baz=bing',
+          message = 'You\'re a derp!';
+      var input = '' +
+        '<a href="${url}?q=${query}" onclick=alert(${message});>${message}</a>';
+      var expected = '<a href="http://www.example.org?foo=bar&baz=bing" ' +
+                        'onclick=alert("You\'re a derp!");>You\'re a derp!</a>';
+      assert.strictEqual(qz(input), expected);
+    });
+
+    it('a.${className}[href=~"//${domain}/"]', function() {
+      var className = 'btn';
+      var domain = 'http://example.org';
+      var input = 'a.${className}[href=~"//${domain}/"]';
+      var expected = 'a.btn[href=~"http://example.org"]';
+      assert.strictEqual(qz(input), expected);
+    });
+
   });
 
 });
